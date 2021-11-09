e1c15efdd6660920c068952646670bcc907eeccc
LemonCaramel <admin@caramel.moe>
2021-11-09 12:57:26 +0900

    add: support hangul

--- src/main/java/com/mojang/brigadier/StringReader.java
+++ src/main/java/com/mojang/brigadier/StringReader.java
@@ -170,6 +170,8 @@ public class StringReader implements ImmutableStringReader {
         return c >= '0' && c <= '9'
             || c >= 'A' && c <= 'Z'
             || c >= 'a' && c <= 'z'
+            || c >= 'ㄱ' && c <= 'ㅎ'
+            || c >= '가' && c <= '힣'
             || c == '_' || c == '-'
             || c == '.' || c == '+';
     }
